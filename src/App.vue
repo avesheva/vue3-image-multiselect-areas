8fdbf10f879fe56d31e5b971ae630cdaa7de0613
avesheva <shevochkandrey@gmail.com>
2022-12-24 10:14:42 +0200

    Add 'image-url' and sizes.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <canvas-redactor />
+  <div>
+    <canvas-redactor
+      image-url="https://www.watermelon.org/wp-content/uploads/2020/07/Frog-Carving-27-1000x1000.jpg"
+      :width="800"
+      :height="700"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
