f3536e9317ceb1236b9993f4c30548511ebbf0a1
avesheva <shevochkandrey@gmail.com>
2022-12-26 14:11:11 +0200

    Add required prop 'imageUrl' and rename test file.

--- tests/CanvasRedactor.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount, VueWrapper } from '@vue/test-utils'
-import LibComponent from '../lib'
-
-test('Renders lib component', () => {
-  const wrapper: VueWrapper = mount(LibComponent as never)
-
-  wrapper.exists()
-})
-
